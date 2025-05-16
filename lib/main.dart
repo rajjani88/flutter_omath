a683327a79f7317df3b24577993b659f54ac3812
Raj Jani <janiraj88@gmail.com>
2025-05-16 11:43:34 +0530

    gradle and project upgrade

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_omath/controllers/fruit_game_controller.dart';
-import 'package:flutter_omath/controllers/game_menu_controller.dart';
-import 'package:flutter_omath/controllers/home_game_contoller.dart';
-import 'package:flutter_omath/utils/consts.dart';
-import 'package:flutter_omath/utils/navigation_router.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_omath/screens/splash/splash.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,23 +11,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => GameMenuController()),
-        ChangeNotifierProvider(create: (_) => HomeGameController()),
-        ChangeNotifierProvider(create: (_) => FruitGameController()),
-      ],
-      child: Builder(
-        builder: (context) => MaterialApp.router(
-          debugShowCheckedModeBanner: false,
-          title: appName,
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-            useMaterial3: true,
-          ),
-          routerConfig: router,
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Brainy Math',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
       ),
+      home: const SplashScreen(),
     );
   }
 }
